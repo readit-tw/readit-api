c90cecfe1b73a9629965e5ecafd43173ce9ff2bc
jayasagar <jayasagar@gmail.com>
2015-08-31 13:29:57 +0530

    Search by tag works

--- repository/mongo_resource_repository.go
+++ repository/mongo_resource_repository.go
@@ -6,6 +6,7 @@ import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"log"
+	//"fmt"
 )
 
 type MongoResourceRepository struct {
@@ -18,8 +19,35 @@ func NewMongoResourceRepository(db *mgo.Database) *MongoResourceRepository {
 func (rr *MongoResourceRepository) SearchByTerm(term string) ([]*model.Resource, error) {
 	var resources []*model.Resource
 	
+	
+	// Get Tag ids matching query term
+	var tags []*model.Tag
+	err := rr.db.C("tags").Find(bson.M{"name": bson.RegEx{term, "i"}}).Select(bson.M{"_id": 1}).All(&tags)
+	
+	tagIds := make([]bson.ObjectId, 0, len(tags))
+	for index := range tags{
+		tagIds = append(tagIds, tags[index].Id)
+	}
+	
+	//fmt.Println("tag objectIds is ", tagIds)
+	
 	//http://stackoverflow.com/questions/3305561/how-to-query-mongodb-with-like
-	err := rr.db.C("resources").Find(bson.M{"title": bson.RegEx{term, "i"}}).All(&resources)
+	//err = rr.db.C("resources").Find(bson.M{"title": bson.RegEx{term, "i"}}).All(&resources)
+	err = rr.db.C("resources").Find(bson.M{ "$or": 
+												[]bson.M{
+														bson.M{
+															"title": 
+															bson.RegEx{term, "i"},
+															}, 
+														bson.M{
+															"tag_id": 
+															bson.M{"$in" : tagIds},
+															},
+														},
+												}).All(&resources)
+	
+	//log.Printf("after serach query ")
+	
 	if err != nil {
 		return nil, errors.New("Failed to Retrieve")
 		log.Printf("Failed to Retrieve search result for :" + term)
